86e8cde77a586015d393c3cf33490714bb51d2fd
henriqueclsilva <rickcorreia@live.com>
2025-05-12 02:27:06 -0300

    Ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,7 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = NODE_ENV === "https://spotify-clone-ixya.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
